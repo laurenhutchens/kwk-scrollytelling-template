7f8984e7eb912692c7326aea4ed9900e1d2d8af1
laurenhutchens <laurensh30@gmail.com>
2025-07-19 16:28:41 -0700

    Changed the TitleSection text

--- src/sections/TitleSection.svelte
+++ src/sections/TitleSection.svelte
@@ -1,8 +1,8 @@
 <script>
     import TitleCard from "../lib/TitleCard.svelte";
     
-    const title = "Welcome to the KWK Scrollytelling Template";
-    const subtitle = "A journey through data, creativity, and code";
+    const title = "Welcome to the KWK Scrollytelling Data Final Project!";
+    const subtitle = "A journey through data, creativity, and codeFollow the Story of Black Wealth, Education, and Business in Yavapai County.";
 </script>
 
 <TitleCard {title} {subtitle} />
