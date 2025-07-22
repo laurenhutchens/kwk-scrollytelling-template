d25f772da29bd547590387d75ac2ba79191aeb63
laurenhutchens <laurensh30@gmail.com>
2025-07-21 19:04:10 -0700

    Updates

--- src/sections/TitleSection.svelte
+++
@@ -1,8 +0,0 @@
-<script>
-    import TitleCard from "../lib/TitleCard.svelte";
-    
-    const title = "Welcome to the KWK Scrollytelling Data Final Project!";
-    const subtitle = "A journey through data, creativity, and codeFollow the Story of Black Wealth, Education, and Business in Yavapai County.";
-</script>
-
-<TitleCard {title} {subtitle} />
